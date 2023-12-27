40f7f081318a54787f4c21b3be0e62d9552f1486
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2023-12-27 15:25:32 -0800

    fix: error checking

--- action-end.sh
+++ action-end.sh
@@ -4,8 +4,9 @@ echo "===> Action end"
 
 # Use saved action token to start it
 RESPONSE=$(curl -s -X POST $BROKER_URL/v1/intention/action/end -H 'X-Broker-Token: '"$ACTION_TOKEN"'')
+code=$?
 
-if [ "$(echo $RESPONSE | jq '.error')" != "null" ]; then
+if [ "$code" != "0" ]; then
     echo "Exit: Error detected"
     echo $RESPONSE | jq '.'
     exit 1
